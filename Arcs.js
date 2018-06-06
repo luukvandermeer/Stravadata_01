1e3ad61725cb8fb58a615dcdcaabad8204c95cce
Luuk <luuk.vandermeer@gmail.com>
2018-06-06 15:25:25 +0200

    Hover, multiple datapoints, opacity

--- Arcs.js
+++ Arcs.js
@@ -1,78 +1,158 @@
-d3.json("test.json", function (data) {
-    console.log(function (d) {
-        return d.rondje * 20;
-    });
+d3.json("test.json", function(data) {
+  //console.log(function(d) {
+  //  return d.rondje * 20;
+  //  });
 
-
-    var jsonRondjes = [
-        {
-            "rondje": 1
+  var jsonRondjes = [{
+      "rondje": 1,
+      "test": "extra",
+      "day": 0.1
     },
-        {
-            "rondje": 2
+    {
+      "rondje": 2,
+      "test": "extra",
+      "day": 0.2
     },
-        {
-            "rondje": 30
+    {
+      "rondje": 3,
+      "test": "extra",
+      "day": 0.3
     },
-        {
-            "rondje": 40
+    {
+      "rondje": 4,
+      "test": "extra",
+      "day": 0.4
     },
-        {
-            "rondje": 50
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.5
     },
-        {
-            "rondje": 60
-    }];
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.6
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.7
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.5
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.6
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.7
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.5
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.6
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.7
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.5
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.6
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.7
+    },
+    {
+      "rondje": 5,
+      "test": "extra",
+      "day": 0.8
+    },
+    {
+      "rondje": 60,
+      "test": "extra",
+      "day": 0.9
+    }
+  ];
 
-    console.log(d3.select("body").append("svg").attr("rondje", 200).data(jsonRondjes).enter()); //code laat array zien in console, jeej
+  console.log(d3.select("body").append("svg").attr("rondje", 200).data(jsonRondjes).enter()); //code laat array zien in console, jeej
 
-    var width = window.innerWidth,
-        height = window.innerHeight;
+  var width = window.innerWidth,
+    height = window.innerHeight,
+    margin = {
+      top: 40,
+      right: 20,
+      bottom: 20,
+      left: 40
+    };
 
-    // append svg to the DIV
-    d3.select(".chart").append("svg:svg")
-        .attr("width", width)
-        .attr("height", height)
+  // append svg to the DIV
+  d3.select(".chart").append("svg:svg")
+    .attr("width", width)
+    .attr("height", height)
 
 
-    var render = function () {
-        vis = d3.select("svg"); // select the svg
+  var render = function() {
+    vis = d3.select("svg"); // select the svg
+
+    // set constants
+    var PI = Math.PI;
+    var arcMin = 10; // inner radius of the first arc
+    var arcWidth = 5; // width
+    var arcPad = 1; // padding between arcs
+    // var le_data = d3.selectAll(data).data(function (d, i) {
+    //console.log(d);
+    //   return d.value
+    //});
+    //  var start_time = [0];    //startdataset
+
+    var arcs = vis.selectAll("path.arc-path")
+      .data(jsonRondjes)
+      ;
 
 
-        // set constants
-        var PI = Math.PI;
-        var arcMin = 10; // inner radius of the first arc
-        var arcWidth = 3; // width
-        var arcPad = 1; // padding between arcs
-        // var le_data = d3.selectAll(data).data(function (d, i) {
-        //console.log(d);
-        //   return d.value
-        //});
-        //  var start_time = [0];    //startdataset
 
-        var arcs = vis.selectAll("path.arc-path").data(jsonRondjes);
+    // arc accessor
+    //  d and i are automatically passed to accessor functions,
+    //  with d being the data and i the index of the data
+    var drawArc = d3.svg.arc()
+      .innerRadius(function(d, i) {
+        return arcMin + i * (arcWidth) + arcPad;
+      })
+      .outerRadius(function(d, i) {
+        return arcMin + (i + 1) * (arcWidth);
+      })
+      .startAngle(0)
+      .endAngle(
+        function(d) {
+          return d.rondje
+        });
 
-        // arc accessor
-        //  d and i are automatically passed to accessor functions,
-        //  with d being the data and i the index of the data
-        var drawArc = d3.svg.arc()
-            .innerRadius(function (d, i) {
-                return arcMin + i * (arcWidth) + arcPad;
-            })
-            .outerRadius(function (d, i) {
-                return arcMin + (i + 1) * (arcWidth);
-            })
-            .startAngle(0)
-            .endAngle(
-                function (d) {
-                    return d.rondje
-                });
+    //Code om endAngel te bewerken return d * 6 * (PI /180)
+    //        * 6 * (PI / 180)
+    // });
 
-        //Code om endAngel te bewerken return d * 6 * (PI /180)
-        //        * 6 * (PI / 180)
-        // });
-
-        /*
+    /*
         // bind the data
         var arcs = vis.selectAll("path.arc-path").data(function (d, i) {
             console.log(d);
@@ -84,88 +164,103 @@ d3.json("test.json", function (data) {
 
 
 
-        //data([10,20,34,23,23,23,45,7,8,56,34,23,23,23,45,7,8,56]);
-        //function (d) {return d.value*10});
+    //data in een array
+    //data([10,20,34,23,23,23,45,7,8,56,34,23,23,23,45,7,8,56]);
+    //function (d) {return d.value*10});
 
-        /* Comment --> overbodige code welke een functie aanroept om de kleuren aan te passen
-                  // *** update existing arcs -- redraw them ***
-                arcs.attr("d", drawArc)
-                    .attr("fill", function(d){
-                        // we need to redefine the fills as well since we have new data,
-                        //  otherwise the colors would no longer be relative to the data
-                        //  values (and arc length). if your fills weren't relative to
-                        //  the data, this would not be necessary
-                      var grn = Math.floor((1 - d/60)*255);
+    /* Comment --> overbodige code welke een functie aanroept om de kleuren aan te passen
+              // *** update existing arcs -- redraw them ***
+            arcs.attr("d", drawArc)
+                .attr("fill", function(d){
+                    // we need to redefine the fills as well since we have new data,
+                    //  otherwise the colors would no longer be relative to the data
+                    //  values (and arc length). if your fills weren't relative to
+                    //  the data, this would not be necessary
+                  var grn = Math.floor((1 - d/60)*255);
+                  return "rgb(0, "+ grn +", 0)";
+                });
+    */
+
+    // draw arcs for new data
+    arcs.enter().append("svg:path")
+      .attr("class", "arc-path") // assigns a class for easier selecting
+      .attr("transform", "translate(400,200)") // sets position--easier than setting x's and y's
+      .attr("fill", "rgb(0, 204, 204)")
+      //functie toegevoegd om de opacity aan te passen afhankelijk van een waarde
+      .attr("opacity", function(d) {
+        return d.day * 7;
+      })
+      //      .on('mouseover', tip.show)
+      //    .on('mouseout', tip.hide)
+
+      /* code om o.b.v. waarde en een functie de kleur aan te passen
+                  .attr("fill", function(d){
+                        // fill is an rgb value with the green value determined by the data
+                        // smaller numbers result in a higher green value (1 - d/60)
+                        // you should also look into using d3 scales to create gradients
+                      var grn = Math.floor((1 - d/60)*15);
                       return "rgb(0, "+ grn +", 0)";
-                    });
-        */
+                    })
+      */
 
-        // draw arcs for new data
-        arcs.enter().append("svg:path")
-            .attr("class", "arc-path") // assigns a class for easier selecting
-            .attr("transform", "translate(400,200)") // sets position--easier than setting x's and y's
-            .attr("fill", "rgb(0, 204, 204)")
-            //      .on('mouseover', tip.show)
-            //    .on('mouseout', tip.hide)
+      .attr("d", drawArc); // draw the arc
+  };
 
-        /* Luuk --> code om o.b.v. waarde en een functie de kleur aan te passen
-                    .attr("fill", function(d){
-                          // fill is an rgb value with the green value determined by the data
-                          // smaller numbers result in a higher green value (1 - d/60)
-                          // you should also look into using d3 scales to create gradients
-                        var grn = Math.floor((1 - d/60)*15);
-                        return "rgb(0, "+ grn +", 0)";
-                      })
-        */
+  /*
+      //Code aangepast waardoor deze functie niet meer nodig is
+      var generateTimes = function (quantity) {
+          var i, times = [];
 
-        .attr("d", drawArc); // draw the arc
-    };
+          for (i = 0; i < quantity; i++) {
+              times.push(Math.round(Math.random() * 60));
+          }
+          return times;
+      };
+  */
 
-    /* 
-        //Code aangepast waardoor deze functie niet meer nodig is
-        var generateTimes = function (quantity) {
-            var i, times = [];
-
-            for (i = 0; i < quantity; i++) {
-                times.push(Math.round(Math.random() * 60));
-            }
-            return times;
-        };
-    */
-
-    // drawing the click area
-    var initialize = function (d, i) {
-        render(i);
-    }
+  // drawing the click area
+  var initialize = function(d, i) {
+    render(i);
+  }
 
 
 
-    /* var initialize = function () {
-            var arcMin = 365 / 2; // this should match the arcMin in render()
-            var times = generateTimes(300);
-            render(times);
-    */
-    /* Comment --> Maakt een innercircle welke niet nodig is
+  /* functie aangepast naar bovestaande initialize
+  var initialize = function () {
+          var arcMin = 365 / 2; // this should match the arcMin in render()
+          var times = generateTimes(300);
+          render(times);
+  */
+  /* Comment --> Maakt een innercircle welke niet nodig is
 
-              // making the click circle
-            if(!d3.selectAll("circle.click-circle")[0].length) {    // if there is no click area..
-              d3.select("svg").append("circle")
-                  .attr("class", 'click-circle')
-                  .attr("transform", "translate(400,200)")
-                  .attr("r", arcMin*0.85)
-                  .attr("fill", "rgba(201, 201, 201, 0.5)")
-                  .on("click", function(d) {
-                    times = generateTimes(6);
-                    render(times);
-                  });
-            }
-    */
+            // making the click circle
+          if(!d3.selectAll("circle.click-circle")[0].length) {    // if there is no click area..
+            d3.select("svg").append("circle")
+                .attr("class", 'click-circle')
+                .attr("transform", "translate(400,200)")
+                .attr("r", arcMin*0.85)
+                .attr("fill", "rgba(201, 201, 201, 0.5)")
+                .on("click", function(d) {
+                  times = generateTimes(6);
+                  render(times);
+                });
+          }
+  */
 
 
-    initialize();
+  initialize();
 
-    d3.selectAll(".arc-path").each(function (d, i) {
-        //   console.log(i);
+  d3.selectAll(".arc-path")
+    .each(function(d, i) {
+            console.log(i);
     })
+    .on("mouseover", function(d,i) {
+      console.log(d);
+      console.log(d.rondje);
+    }
+  )
+    ;
+
+
 
 });
